6618fee66fc1b66313297a716598b1482d0b8a07
n-tien <n-tien@atware.asia>
2026-02-03 15:38:22 +0700

    add step 2, step 3 & step 4

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Step1 from "./components/step-1";
 import Step2 from "./components/step-2";
 import Step3 from "./components/step-3";
 import Step4 from "./components/step-4";
+import dishData from "./data/dishes.json";
 
 interface OrderData {
   meal: string;
@@ -19,6 +20,18 @@ const App: React.FC = () => {
     restaurant: "",
     dishes: [],
   });
+
+  const filteredRestaurants = Array.from(
+    new Set(
+      dishData.dishes
+        .filter((d) => d.availableMeals.includes(formData.meal))
+        .map((d) => d.restaurant),
+    ),
+  );
+
+  const filteredDishes = dishData.dishes.filter(
+    (d) => d.restaurant === formData.restaurant,
+  );
 
   const updateData = (newData: Partial<OrderData>) => {
     setFormData((prev) => ({ ...prev, ...newData }));
@@ -41,9 +54,25 @@ const App: React.FC = () => {
           onNext={handleNext}
         />
       )}
-      {step === 2 && <Step2 />}
-      {step === 3 && <Step3 />}
-      {step === 4 && <Step4 />}
+      {step === 2 && (
+        <Step2
+          formData={formData}
+          updateData={updateData}
+          onNext={handleNext}
+          onBack={handleBack}
+          restaurants={filteredRestaurants}
+        />
+      )}
+      {step === 3 && (
+        <Step3
+          formData={formData}
+          updateData={updateData}
+          onNext={handleNext}
+          onBack={handleBack}
+          availableDishes={filteredDishes}
+        />
+      )}
+      {step === 4 && <Step4 formData={formData} onBack={handleBack} />}
     </main>
   );
 };
